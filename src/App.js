bc9016f3dde35deaaee6ee4fc7c3bae68f335fcd
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-24 15:42:27 +0200

    Added Container component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Container from './components/Container/Container';
 import Profile from './components/Profile/Profile';
 
 import { name, tag, location, avatar, stats } from './db/user.json';
@@ -6,14 +7,16 @@ import { name, tag, location, avatar, stats } from './db/user.json';
 function App() {
   return (
     <div>
-      <h1>Hello, I am future Homework!</h1>
-      <Profile
-        name={name}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-        stats={stats}
-      />
+      <Container>
+        <h1>React Homework. Module 1.</h1>
+        <Profile
+          name={name}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          stats={stats}
+        />
+      </Container>
     </div>
   );
 }
