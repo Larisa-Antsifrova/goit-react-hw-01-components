102d0a759e61ba962591c93dbf3003ec31e80a97
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-24 23:08:39 +0200

    Added TransactionHistory component and its styles.

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -3,31 +3,41 @@ import PropTypes from 'prop-types';
 
 import styles from './TransactionHistory.module.css';
 
-const TransactionHistory = () => {
+const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history">
-      <thead>
+    <table className={styles['transaction-history']}>
+      <thead className={styles['table-header']}>
         <tr>
           <th>Type</th>
-          <th>Amount</th>
+          <th className={styles.middle}>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
+        {items.map(({ id, type, amount, currency }, index) => (
+          <tr key={id} className={index % 2 ? styles.odd : styles.even}>
+            <td className={styles.type}>
+              {type[0].toUpperCase() + type.slice(1)}
+            </td>
+            <td className={styles.middle}>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 export default TransactionHistory;
