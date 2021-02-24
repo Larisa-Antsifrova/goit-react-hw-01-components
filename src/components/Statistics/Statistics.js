19b385a3426d2f8c125c1bde3cea96976641c400
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-25 00:21:59 +0200

    Added random color function source.

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
 // Function for getting random color in HEX format
+// Source: https://css-tricks.com/snippets/javascript/random-hex-color/
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
