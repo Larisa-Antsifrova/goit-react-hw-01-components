e9d71c42e53eb4326f9c5208e6d5bbce11b5a46a
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-24 15:29:33 +0200

    Added basic Profile component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import Profile from './components/Profile/Profile';
+
+import { name, tag, location, avatar, stats } from './db/user.json';
+
 function App() {
   return (
     <div>
       <h1>Hello, I am future Homework!</h1>
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        // avatar={avatar}
+        stats={stats}
+      />
     </div>
   );
 }
