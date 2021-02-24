121649a3a8812c4704f0b0588ddefd7b5306b77d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-24 16:24:57 +0200

    Added Title vs PageTitle component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Section from './components/Section/Section';
 
 import PageTitle from './components/PageTitle/PageTitle';
+import Title from './components/Title/Title';
 import Profile from './components/Profile/Profile';
 
 import { name, tag, location, avatar, stats } from './db/user.json';
@@ -11,6 +12,7 @@ function App() {
     <Fragment>
       <PageTitle text="React Homework. Module 1." />
       <Section>
+        <Title text="Task One. Profile." />
         <Profile
           name={name}
           tag={tag}
